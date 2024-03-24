9787054effa5c124a310f29b3a782352a1d70d1c
NaufalAjhar <74417421+NaufalAjhar@users.noreply.github.com>
2024-03-24 20:33:32 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302223040/Program.cs
+++ tpmodul6_1302223040/Program.cs
@@ -1,10 +1,35 @@
 ﻿namespace tpmodul6_1302223040
 {
+    public class SayaTubeVideo
+    {
+        public int Id;
+        public string Title;
+        public int PlayCount;
+
+        public SayaTubeVideo(string title)
+        {
+            Random random = new Random();
+            this.Id = random.Next(10000, 99999);
+            this.Title = title;
+            this.PlayCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            this.PlayCount += count;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("ID: " + this.Id + " Title: " + this.Title + " Play Count: " + this.PlayCount);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Naufal Ajhar");
+            video.PrintVideoDetails();
         }
     }
 }
