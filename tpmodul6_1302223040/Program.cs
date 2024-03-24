7e6ae49107e24dfd685bead71dbbc147cc33d141
NaufalAjhar <74417421+NaufalAjhar@users.noreply.github.com>
2024-03-24 21:09:12 +0700

    implementasi design by contract

--- tpmodul6_1302223040/Program.cs
+++ tpmodul6_1302223040/Program.cs
@@ -8,6 +8,11 @@
 
         public SayaTubeVideo(string title)
         {
+            if (title == null || title.Length > 100)
+            {
+                throw new ArgumentException("title tidak boleh null dan harus kurang dari 100 karakter");
+            }
+
             Random random = new Random();
             this.Id = random.Next(10000, 99999);
             this.Title = title;
@@ -16,7 +21,22 @@
 
         public void IncreasePlayCount(int count)
         {
-            this.PlayCount += count;
+            if (count > 10000000)
+            {
+                throw new ArgumentException("Count tidak bisa lebih dari 10,000,000.");
+            }
+
+            try
+            {
+                checked
+                {
+                    this.PlayCount += count;
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("overflow.");
+            }
         }
 
         public void PrintVideoDetails()
@@ -24,11 +44,18 @@
             Console.WriteLine("ID: " + this.Id + " Title: " + this.Title + " Play Count: " + this.PlayCount);
         }
     }
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
             SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Naufal Ajhar");
+            video.PrintVideoDetails();
+            for (int i = 0; i < 10000000; i++)
+            {
+                video.IncreasePlayCount(1);
+            }
             video.PrintVideoDetails();
         }
     }
